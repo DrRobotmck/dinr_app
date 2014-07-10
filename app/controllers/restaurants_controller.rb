4dbd6639be84bc558ae1e752e6fb218840bf478c
DrRobotmck <mckenneth.scott@icloud.com>
2014-07-10 09:37:38 -0400

    edits, turbolinks bug found

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,6 @@ class RestaurantsController < ApplicationController
 	def index
 		ip_results = Geocoder.search(request.ip)[0].data
 		session[:ip_address] = [ip_results['latitude'], ip_results['longitude']]
-		p session[:ip_address]
 	end
 
 	def show
@@ -11,21 +10,20 @@ class RestaurantsController < ApplicationController
 		@yelp_results = Yelp.search(@restaurant)
 		@inspection_results = Inspection.where(restaurant_id: @restaurant.id).group_by(&:inspected_on)
 		@boro = { '1' => 'Manhattan', '2' => 'Bronx', '3' => 'Brooklyn', '4' => 'Queens', '5' => 'Staten Island'}
+		respond_to { |format| format.js {} }
 	end
 
 	def search
 		@restaurants = Restaurant.search(params[:zip_or_name])
 		@restaurant_coords = session[:ip_address]
-		respond_to do |format|
-			format.js {}
-		end
+		respond_to { |format| format.js {} }
 	end
 
 	def favorite
 		@restaurant = Restaurant.find(params[:id])
 		current_user.favorite(@restaurant)
 		respond_to do |format|
-			format.html {render nothing: true}
+			format.html { render nothing: true }
 			format.js {}
 		end
 	end
@@ -34,7 +32,7 @@ class RestaurantsController < ApplicationController
 		@restaurant = Restaurant.find(params[:id])
 		current_user.unfavorite(@restaurant)
 		respond_to do |format|
-			format.html {render nothing: true}
+			format.html { render nothing: true }
 			format.js {}
 		end
 	end
